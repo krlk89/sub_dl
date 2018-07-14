48367133b2cce062e467eff34733ea21b7b0536c
krlk89 <kaarel.koitne@gmail.com>
2018-07-14 18:26:26 +0300

    cleanup

--- sub_dl_config.py
+++ sub_dl_config.py
@@ -13,23 +13,20 @@ def create_config(path):
 
     is_media_dir = Path(directory).is_dir()
     if not is_media_dir:
-        print("Specified media directory does not exist. Press enter to create the directory or specify a new one.")
+        print("Specified media directory does not exist. Try again.")
 
     while not is_media_dir:
         directory = input("Type your media directory: ")
         is_media_dir = Path(directory).is_dir()
 
-    print(Path(directory))
-    return
+    config.set("Settings", "dir", directory)
 
-    config.set("Settings", "dir", directory) # add check if directory exists (or create directory?)
-    
     with open(str(path), "w") as config_file:
         config.write(config_file)
-            
+
 def read_config(path):
     """Read configuration file."""
     config = configparser.ConfigParser()
     config.read(str(path))
-    
+
     return config.get("Settings", "dir")
